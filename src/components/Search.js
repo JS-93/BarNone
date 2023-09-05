d7a22c2c0c41368a90523564ec02b4e8a08f8f5c
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-05 15:09:58 -0700

    Update Search.js
    added searchMade/cocktails.length condition to prevent errors from popping up when incorrect cocktail is entered

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,19 @@
 import CocktailList from "./CocktailList"
 
-const Search = ({ handleSearch, handleChange, search, cocktails }) => {
+
+
+
+const Search = ({ handleSearch, handleChange, search, cocktails, searchMade }) => {
+    
+   
+    
     return (<>
     
        <form className="form" onSubmit={handleSearch}>
-        <input className="text" value={search} onChange={handleChange} type="text"></input>
+        <input className="text" placeholder="Please enter a cocktail name..." value={search} onChange={handleChange} type="text"></input>
         <input className="button" type="submit" value="Search Cocktail"></input>
-       </form>
-       <div><CocktailList cocktails={cocktails}/></div>
+       </form>{(searchMade && cocktails.length === 0) ? (<p>Please enter correct cocktail info</p>) : (<div><CocktailList cocktails={cocktails}/></div>)}
+       
        </>
     )
 }
