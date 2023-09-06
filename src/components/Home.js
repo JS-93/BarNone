2f95b34f4dff185a2a86fe27fed379a260a5baec
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-06 11:57:41 -0700

    Update Home.js
    Added homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+import { useEffect } from 'react'
+
 
 const Home = () => {
+
+useEffect(() => {
+    const logo = document.querySelector(".logo")
+    if (logo) {
+        setTimeout(() => {
+            logo.classList.add("logoHighlight")
+        }, 1500)
+    }
+}, [])
     return (
-        <h1>Home Page</h1>
+        <div className="homepage">
+        <h1 className="logo">barNone</h1>
+        <p>A fun way to  
+            <Link to="/search" style={{cursor: 'pointer'}}><span> DISCOVER </span></Link>
+             new drinks and 
+             <Link to="/form" style={{cursor: 'pointer'}}>
+             <span> CREATE </span></Link> your own!</p>
+        </div>
     )
 }
 
