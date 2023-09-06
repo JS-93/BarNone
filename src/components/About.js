28c4f70aec81666e3de9687390f0ddcdedc4a510
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-06 14:53:16 -0700

    Update About.js
    add Menu to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
-const About = () => {
+import { useState, useEffect } from 'react'
+import Menu from './Menu'
 
+
+const About = () => {
+    const [cocktails, setCocktails] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3000/createdDrinks')
+        .then(resp => resp.json())
+        .then(setCocktails)
+
+    }, [])
+    
     return (
-        <>
-            <h1>Created Drinks</h1>
-            
-        </>
+        <div className="background">
+        <div className="menuContainer">
+            <u><h3 className="menuTitle">Drink Menu</h3></u>
+            <Menu cocktails={cocktails}/>
+
+        </div>
+        </div>
+        
     )
 }
 
