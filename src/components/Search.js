ffa576b3ab879978947c5e52d193ed958ef1b780
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-07 12:23:14 -0700

    Update Search.js
    Restructured search bar to handle css

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,12 +8,15 @@ const Search = ({ handleSearch, handleChange, search, cocktails, searchMade }) =
    
     
     return (<>
-    
-       <form className="form" onSubmit={handleSearch}>
-        <input className="text" placeholder="Please enter a cocktail name..." value={search} onChange={handleChange} type="text"></input>
-        <input className="button" type="submit" value="Search Cocktail"></input>
-       </form>{(searchMade && cocktails.length === 0) ? (<p>Please enter correct cocktail info</p>) : (<div><CocktailList cocktails={cocktails}/></div>)}
-       
+    <div className="searchBackground">
+       <form className="searchBar" onSubmit={handleSearch}>
+        <input className="searchInput" placeholder="Please enter a cocktail name..." value={search} onChange={handleChange} type="text"></input>
+        <input className="searchButton" type="submit" value="Search Cocktail"></input>
+       </form>
+       <div className="searchCardContainer">
+       {(searchMade && cocktails.length === 0) ? (<p>Please enter correct cocktail info</p>) : (<div><CocktailList cocktails={cocktails}/></div>)}
+       </div>
+       </div>
        </>
     )
 }
