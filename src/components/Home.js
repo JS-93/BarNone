534f1976e4d321387f1fee961de0d6ba3000ca4a
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-07 12:19:10 -0700

    Update Home.js
    No need for UseEffect on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,9 @@
 import { Link } from "react-router-dom"
-import { useEffect } from 'react'
+
 
 
 const Home = () => {
 
-useEffect(() => {
-    const logo = document.querySelector(".logo")
-    if (logo) {
-        setTimeout(() => {
-            logo.classList.add("logoHighlight")
-        }, 1500)
-    }
-}, [])
     return (
         <div className="homepage">
         <h1 className="logo">barNone</h1>
