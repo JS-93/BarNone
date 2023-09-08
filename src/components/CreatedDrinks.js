adb0a0f93a5d1f78b841a9ce9b5b60833445a775
JS-93 <137359185+JS-93@users.noreply.github.com>
2023-09-07 19:05:41 -0700

    Update CreatedDrinks.js
    touched up for final draft

--- src/components/CreatedDrinks.js
+++ src/components/CreatedDrinks.js
@@ -9,7 +9,7 @@ const CreatedDrinks = ({cocktails, onDelete}) => {
   const showCreated =  cocktails.map((cocktail)=> (
  <NewDrinkCard key={cocktail.id} cocktail={cocktail} onDelete={onDelete}/>))
 
-return (<><h1>Your Drinks</h1>
+return (<><h1 className="yourDrinksHeader">Your Drinks</h1>
   <div className='cardContainer'>
       {showCreated}
   </div></>
